524a4553287cafde6e0e2c84f16a65bb0e452903
Lorenzo Zanetti - lorenzo.zanetti5@studio.unibo.it <lorenzo.zanetti5@studio.unibo.it>
2020-11-10 15:46:12 +0100

    Anonymous1 resolved

--- src/it/unibo/oop/lab/anonymous1/TestAnonymousComparator.java
+++ src/it/unibo/oop/lab/anonymous1/TestAnonymousComparator.java
@@ -1,6 +1,8 @@
 package it.unibo.oop.lab.anonymous1;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import it.unibo.oop.lab.socialnetwork.SocialNetworkUser;
@@ -74,7 +76,12 @@ public final class TestAnonymousComparator {
          * 
          * REFER TO LESSON 13-Advanced-Mechanisms.pdf, slide 41
          */
-        // TODO
+        denzelUsers.sort(new Comparator<User>() {
+        	public int compare(final User u1, final User u2) {
+        		return u1.getAge() - u2.getAge();
+        	}
+        });
+        
         /*
          * expected Result
          */
@@ -105,7 +112,11 @@ public final class TestAnonymousComparator {
          * NOTE: in order to sort a list think about a method of the utility
          * class Collections
          */
-        // TODO
+        denzelUsers.sort(new Comparator<User>() {
+        	public int compare(final User u1, final User u2) {
+        		return u2.getAge() - u1.getAge();
+        	}
+        });
         /*
          * expected Result
          */
