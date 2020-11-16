6e9d3af54992e6e00f7db035e9c76ac98f17634c
zanetti33 <73821443+zanetti33@users.noreply.github.com>
2020-11-16 19:42:26 +0100

    Update OneListAcceptable.java

--- src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
+++ src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
@@ -17,24 +17,18 @@ public class OneListAcceptable<T> implements Acceptable<T> {
 			private final Iterator<T> iterator = OneListAcceptable.this.listToAccept.iterator();
 			
 			public void accept(T newElement) throws Acceptor.ElementNotAcceptedException {
-				try {
-					if(newElement.equals(this.iterator.next())) {
-	                    			return;
-					}
-                		}catch(Exception e) {
+				if(this.iterator.hasNext() && newElement.equals(this.iterator.next())) {
+	                    		return;
+				}
                     		System.out.println("this element doesn't belong in the list");
                     		throw new Acceptor.ElementNotAcceptedException(newElement);
-                		}
 			}
 
 			public void end() throws Acceptor.EndNotAcceptedException {
-				try{
-					if (!this.iterator.hasNext()) {
-                        			return;
-					}
-                		}catch(Exception e) {
+				if (!this.iterator.hasNext()) {
+                        		return;
+				}
                     		System.out.println("the list is not finished");
-                		}
 				throw new Acceptor.EndNotAcceptedException();
 			}
 			
